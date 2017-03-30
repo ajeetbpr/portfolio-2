12337740d69d5311b02d8a0dcfe73eae1713ec89
shea12 <cshearun12@gmail.com>
2017-03-29 20:02:50 -0500

    add bootstrap.min to public, require in client index file

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,9 +5,15 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import $ from 'jquery'
+import Tether from 'tether'
 
 import App from '../shared/app'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
